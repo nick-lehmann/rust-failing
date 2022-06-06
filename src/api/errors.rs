c5fee3656a759985c57f1ee4149b96117d478882
Nick Lehmann <nick@lehmann.sh>
2022-06-06 15:10:25 +0200

    Implement api error

--- src/api/errors.rs
+++ src/api/errors.rs
@@ -1,7 +1,9 @@
 use crate::service::errors::ServiceError;
+use anyhow::anyhow;
 
 // Notes:
 // - We do not expose the retry error. If a retry error occurs, we retry and the operation either succeeds or becomes an unexpected error.
+#[derive(Debug)]
 pub enum ApiError {
     // User has entered invalid data.
     ValidationError(String),
@@ -11,4 +13,15 @@ pub enum ApiError {
     Unexpected(anyhow::Error),
 }
 
-pub type ApiResult<T> = Result<T, ServiceError>;
+pub type ApiResult<T> = Result<T, ApiError>;
+
+impl From<ServiceError> for ApiError {
+    fn from(e: ServiceError) -> Self {
+        match e {
+            ServiceError::ValidationError(_) => ApiError::ValidationError("ValidationError".into()),
+            ServiceError::Forbidden() => ApiError::Forbidden("Forbidden".into()),
+            ServiceError::Recoverable(e) => ApiError::Unexpected(anyhow!(e)),
+            ServiceError::Unexpected(e) => ApiError::Unexpected(anyhow!(e)),
+        }
+    }
+}
--- src/api/user.rs
+++ src/api/user.rs
@@ -5,8 +5,83 @@ use super::errors::ApiResult;
 
 fn get_user_handler(api_input: ApiInput) -> ApiResult<Option<i32>> {
     let input = validate_input(api_input)?;
-    get_user(input.id)
+    Ok(get_user(input.id)?)
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use crate::{
+        api::errors::ApiError,
+        state::{reset_state, DatabaseState, STATE},
+    };
+
+    use super::*;
+    use std::collections::HashMap;
+
+    #[test]
+    fn test_valid() {
+        reset_state();
+
+        let api_input = HashMap::from([("id".into(), "10".to_string())]);
+        let response = get_user_handler(api_input).unwrap();
+
+        assert_eq!(response, Some(10));
+    }
+
+    #[test]
+    fn test_invalid_input() {
+        reset_state();
+
+        let api_input = HashMap::from([("id".into(), "foo".to_string())]);
+        let response = get_user_handler(api_input);
+        let error = response.unwrap_err();
+
+        match error {
+            ApiError::ValidationError(_) => "",
+            _ => panic!("Expected ValidationError, instead returned: {:?}", error),
+        };
+    }
+
+    #[test]
+    fn test_forbidden() {
+        reset_state();
+
+        let api_input = HashMap::from([("id".into(), "1".to_string())]);
+        let response = get_user_handler(api_input);
+        let error = response.unwrap_err();
+
+        match error {
+            ApiError::Forbidden(_) => "",
+            _ => panic!("Expected Forbidden, instead returned: {:?}", error),
+        };
+    }
+
+    // TODO: Might need modification after retry is implemented.
+    #[test]
+    fn test_unreachable() {
+        reset_state();
+        STATE.lock().unwrap().database_state = DatabaseState::Unreachable();
+
+        let api_input = HashMap::from([("id".into(), "10".to_string())]);
+        let response = get_user_handler(api_input);
+
+        match response {
+            Err(ApiError::Unexpected(_)) => "",
+            _ => panic!("Expected Unexpected, instead returned: {:?}", response),
+        };
+    }
+
+    #[test]
+    fn test_unexpected() {
+        reset_state();
+        STATE.lock().unwrap().database_state = DatabaseState::DatabaseMissing();
+
+        let api_input = HashMap::from([("id".into(), "10".to_string())]);
+        let response = get_user_handler(api_input);
+
+        match response {
+            Err(ApiError::Unexpected(_)) => "",
+            _ => panic!("Expected Unexpected, instead returned: {:?}", response),
+        };
+    }
+}
